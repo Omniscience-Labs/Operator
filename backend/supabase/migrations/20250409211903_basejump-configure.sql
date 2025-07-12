a285c3beca6ccf7350813f8475fbcc49b2c1a9fa
Sundaram Manickam <thundermms123@gmail.com>
2025-07-12 12:51:07 -0400

    deleted all migrations

--- backend/supabase/migrations/20250409211903_basejump-configure.sql
+++
@@ -1,6 +0,0 @@
-UPDATE basejump.config SET enable_team_accounts = TRUE;
-UPDATE basejump.config SET enable_personal_account_billing = TRUE;
-UPDATE basejump.config SET enable_team_account_billing = TRUE;
-
--- Remove duplicate config rows immediately
-DELETE FROM basejump.config WHERE ctid NOT IN (SELECT min(ctid) FROM basejump.config);
